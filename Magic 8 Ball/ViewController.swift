a4f0e6bc42c1785d28e620e22f3e497bb9e518c8
OliverThomsen <s154023@student.dtu.dk>
2018-01-24 21:29:25 +0100

    the magic 8 ball will now answer any question with very high precision

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var ballImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,18 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+        shakeBall()
+    }
+    @IBAction func onShakeButtonPressed(_ sender: UIButton) {
+        shakeBall()
+    }
+    
+    func shakeBall() {
+        let randomNumber = Int(arc4random_uniform(5)) + 1
+        ballImageView.image = UIImage(named: "ball\(randomNumber)")
+        print(randomNumber)
+    }
+    
 }
 
